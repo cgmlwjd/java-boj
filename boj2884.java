342b2eb33fbb48cec8b06645639af895092acd21
Choe huijeong <chj06190306@gmail.com>
2022-06-29 22:22:52 +0900

    1~2day commit

---
+++ boj2884.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class boj2884 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        if(H == 0){
+            if(M < 45){
+                H = 23;
+                M = M + 60 - 45;
+                System.out.printf("%d %d", H, M);
+            }
+            else{
+                if(M >= 45){
+                    M = M - 45;
+                    System.out.printf("%d %d", H, M);
+                }
+            }
+        }
+
+        else {
+            if(H <= 23){
+                if(M < 45){
+                    H = H - 1;
+                    M = M + 60 - 45;
+                    System.out.printf("%d %d", H, M);
+                }
+                else{
+                    if(M >= 45){
+                        M = M - 45;
+                        System.out.printf("%d %d", H, M);
+                    }
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
