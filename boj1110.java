bc5d95f24b0ba1f5df58deb4296bbdfe1bb9f4b2
Choe huijeong <chj06190306@gmail.com>
2022-06-30 21:09:03 +0900

    3day commit

---
+++ boj1110.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class boj1110 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = 0;
+        int cnt = 0;
+
+        int n = sc.nextInt();
+        num = n;
+
+        while(true){
+            int a = n / 10;
+            int b = n % 10;
+            int c = (a + b) % 10;
+            int d = (b * 10) + c;
+
+            n = d;
+            cnt++;
+            if(num == d){
+                break;
+            }
+        }
+
+        System.out.println(cnt);
+
+        sc.close();
+
+    }
+}
